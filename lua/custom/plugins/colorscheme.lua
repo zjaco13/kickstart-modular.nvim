f60c4347ac7f1bd21c3992bf9321e8345ea71582
Zach Jacobson <jacobsonzacharym@gmail.com>
2024-04-09 00:16:49 -0400

    fix for new changes

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    -- Theme inspired by Atom
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+  },
+}
---
+++ lua/custom/plugins/treesitter.lua
@@ -0,0 +1,6 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  opts = {
+    ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'rust', 'go', 'python', 'typescript' },
+  },
+}
